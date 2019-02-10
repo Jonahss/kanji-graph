e103edd41423cf9e92588ff9ed6ab8b5511198b0
Jonah Stiennon <jonahss@gmail.com>
2019-02-10 18:04:20 +0900

    notes and early analysis

--- analysis/most-frequent-kanji.js
+++ analysis/most-frequent-kanji.js
@@ -5,7 +5,7 @@ let orderedByFrequency = graph.query(`MATCH (w:word)-[r:writtenWith]->(k:kanji) 
 
 orderedByFrequency.then(async (results) => {
   results = results.filter((res) => {
-    return res.c > 30
+    return res.c > 20
   })
   results.forEach((res) => {
     console.log(`${res['k.character']} found in ${res.c} words`)
@@ -14,9 +14,9 @@ orderedByFrequency.then(async (results) => {
   console.log('total', results.length)
 
   let topTen = results.slice(0, 10)
-  let whereClauseA = topTen.slice(0, topTen.length/2).map((res) => `k.character = '${res['k.character']}'`).join(' OR ')
-  let whereClauseB = topTen.slice(topTen.length/2).map((res) => `kk.character = '${res['k.character']}'`).join(' OR ')
-  results = await graph.query(`MATCH (kk:kanji)<-[]-(w:word)-[]->(k:kanji) WHERE (${whereClauseA}) AND (${whereClauseB}) RETURN w.kanji, w.meaning`)
+  let whereClauseA = topTen.map((res) => `k.character = '${res['k.character']}'`).join(' OR ')
+  let whereClauseB = topTen.map((res) => `kk.character = '${res['k.character']}'`).join(' OR ')
+  results = await graph.query(`MATCH (kk:kanji)<-[]-(w:word)-[]->(k:kanji) WHERE (${whereClauseA}) AND (${whereClauseB}) AND (k.character != kk.character) RETURN w.kanji, w.meaning`)
 
   results.forEach((res) => {
     console.log(`${res['w.kanji']} ${res['w.meaning']}`)
--- analysis/notes.md
+++ analysis/notes.md
@@ -3,3 +3,5 @@ ideas:
 - curve of frequency, with standard deviation
 - total words unlocked as kanji added in order of top use. whats this curve look like? when do we hit diminishing returns?
 - what percent of kanji own 50% of the words?
+
+5906 total words, made up of 1916 kanji
