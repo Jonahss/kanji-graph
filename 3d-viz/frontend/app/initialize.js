138103db7e1d3ad2dbc2af31db14c7e51d00266d
Jonah Stiennon <jonahss@gmail.com>
2019-02-17 17:00:31 +0900

    3d visualization a little closer. have graph data in d3 format

--- 3d-viz/frontend/app/initialize.js
+++ 3d-viz/frontend/app/initialize.js
@@ -1,6 +1,79 @@
 require('babel-polyfill')
 let _ = require('lodash')
+let ForceGraph3D = require('3d-force-graph')
 
+// redis objects looks like
+// {
+//   ID(w),
+//   w.kanji,
+//   labels(w),
+//   ID(k),
+//   k.character,
+//   labels(k)
+// }
+// and for d3 we want nodes/links arrays
+// {
+//     "nodes": [
+//         {
+//           "id": "id1",
+//           "name": "name1",
+//           "val": 1
+//         },
+//         {
+//           "id": "id2",
+//           "name": "name2",
+//           "val": 10
+//         },
+//         (...)
+//     ],
+//     "links": [
+//         {
+//             "source": "id1",
+//             "target": "id2"
+//         },
+//         (...)
+//     ]
+// }
+let convertToD3GraphData = (rawRedisResults) => {
+  let d3GraphData = {
+    nodes: [],
+    links: []
+  }
+
+  let nodesAsHash = {}
+
+  for (let redisResult of rawRedisResults) {
+    // if word is not yet defined, add it to node list
+    if (!nodesAsHash[redisResult['ID(w)']]) {
+      nodesAsHash[redisResult['ID(w)']] = {
+        id: redisResult['ID(w)'],
+        val: 1,
+        name: redisResult['w.kanji'],
+        color: 'cyan'
+      }
+    }
+
+    // if kanji is not yet deinfed, add it to node list
+    if (!nodesAsHash[redisResult['ID(k)']]) {
+      nodesAsHash[redisResult['ID(k)']] = {
+        id: redisResult['ID(k)'],
+        val: 1,
+        name: redisResult['k.character'],
+        color: 'yellow'
+      }
+    }
+
+    // add relationship to links list
+    d3GraphData.links.push({
+      source: redisResult['ID(w)'],
+      target: redisResult['ID(k)']
+    })
+  }
+
+  d3GraphData.nodes = _.values(nodesAsHash)
+
+  return d3GraphData
+}
 
 document.addEventListener('DOMContentLoaded', async () => {
   let windowId = 'id' + Math.floor((Math.random() * 10000000));
@@ -10,14 +83,14 @@ document.addEventListener('DOMContentLoaded', async () => {
     console.log('socket open');
     socket.onmessage = (msg) => {
       console.log(msg)
-      nodes = [];
-      links = [];
       let data = JSON.parse(msg.data);
 
       if (data.status != 'ok') {
         throw new Error(`response from server did not have status: 'ok'. message: ${data.message}`)
       }
 
+      let graphData = convertToD3GraphData(data.results)
+      console.log(graphData)
 
     // if (data.status === 'ok') {
       //   let relationships = data.results;
