e870bfad9160c444d7d8ed00a9361d6bd472b7a8
Jonah Stiennon <jonahss@gmail.com>
2019-07-28 00:46:43 -0700

    fix tests

--- db-population/add-word.test.js
+++ db-population/add-word.test.js
@@ -2,8 +2,9 @@ let test = require('ava')
 let _ = require('lodash')
 let RedisGraph = require('ioredisgraph')
 
-test.beforeEach(t => {
+test.beforeEach(async t => {
   t.context.graph = new RedisGraph(`test:${t.title.match(/beforeEach hook for (.*)/)[1]}`)
+  await t.context.graph.query(`CREATE (:genesis)`)
   t.context.addWord = require('./add-word.js')(t.context.graph)
 })
 
@@ -23,16 +24,20 @@ test('add-word', async t => {
   let results = await t.context.addWord(word)
 
   let kanjiNodes = await t.context.graph.query(`MATCH (k:kanji) RETURN k`)
-  t.true(_.difference(kanjiNodes.map(n => n['k.character']), ['片', '付']).length == 0)
+  t.true(_.difference(kanjiNodes.map(n => n.k.character), ['片', '付']).length == 0)
 
   let expectedWord = {
-    'w.kanji': '片付く',
-    'w.meaning': 'to put in order,to dispose of,to solve',
-    'w.pronunciation': 'かたづく',
+    w: {
+      kanji: '片付く',
+      meaning: 'to put in order,to dispose of,to solve',
+      pronunciation: 'かたづく',
+    }
   }
 
   let wordNodes = await t.context.graph.query(`MATCH (w:word) RETURN w`)
-  t.deepEqual(wordNodes[0], expectedWord)
+  t.is(wordNodes[0].kanji, expectedWord.kanji)
+  t.is(wordNodes[0].meaning, expectedWord.meaning)
+  t.is(wordNodes[0].pronunciation, expectedWord.pronunciation)
   t.true(wordNodes.length == 1)
 
   let connections = await t.context.graph.query(`MATCH (:word)-[e:writtenWith]->(:kanji) RETURN e`)
