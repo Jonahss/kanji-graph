692c3661a8860c7ccd7d4de7fe3f927d4096c693
Jonah Stiennon <jonahss@gmail.com>
2019-07-28 00:32:12 -0700

    begin optimal order, update redisgraph version and client. found an issue where duplicate relationships are being created

---
+++ db-population/assign-optimal-order.js
@@ -0,0 +1,23 @@
+let RedisGraph = require('ioredisgraph')
+
+let graph = new RedisGraph('kanji')
+
+async function getFirst () {
+  let res = await graph.query(`MATCH (w:word)-[r:writtenWith]->(k:kanji) RETURN k.character, id(k), count(w.kanji) as c order by c desc LIMIT 1`)
+  let first = res[0]
+  return first
+}
+
+async function getWords (id) {
+  let res = await graph.query(`MATCH (k2:kanji)<-[r]-(w:word)-[]->(k:kanji) WHERE id(k) = ${id} AND id(k2) != id(k) AND id(w) = 3013 RETURN w.kanji, w.meaning, r`)
+  return res
+}
+
+async function run () {
+  let first = await getFirst()
+  let words = await getWords(first['id(k)'])
+  console.log(words)
+  graph.quit()
+}
+
+run()
